5f3da4498be822f234674f988f19312b0043c068
Alperen Aga <alperencode@gmail.com>
2021-12-11 22:06:36 +0300

    Made snapzones again with prefab for flexibility

---
+++ Main_Demo/Assets/SnapzoneTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapzoneTest : MonoBehaviour
+{
+    public static void SnapzoneTrigger()
+    {
+        Debug.Log("Snapzone triggered");
+    }
+
+}
