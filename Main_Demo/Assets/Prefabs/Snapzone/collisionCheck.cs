5de7eac958c73797a1bfae19cdf99aedc252d593
Alperen Aga <alperencode@gmail.com>
2021-12-26 00:58:21 +0300

    CollisionCheck script is working Led emission control function is working Add PowerCable Check control for led is **not** working

--- Main_Demo/Assets/Prefabs/Snapzone/collisionCheck.cs
+++ Main_Demo/Assets/Prefabs/Snapzone/collisionCheck.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class collisionCheck : MonoBehaviour
 {
+
+    public GameObject led;
+
     private void Update()
     {
         // Checking if the script working
-        Debug.Log("Collision script is active");
+        // Debug.Log("Collision script is active");
     }
 
-
-    public void OnCollisionEnter(Collision collision)
+    private void OnTriggerStay(Collider other)
     {
-        // Need to check current game object
-        Debug.Log("Collided with " + collision.collider.name);
+        if(other.gameObject.name == "led")
+        {
+            Debug.Log("LedCheck bool need to be true");
+            led.GetComponent<LedScript>().LedCheck = true;
+        }else if(other.gameObject.name == "PowerCable")
+        {
+            Debug.Log("PowerCheck bool need to be true");
+            led.GetComponent<LedScript>().PowerCheck = true;
+        }
     }
+
 
 
 }
